05c7eb18067b1b2500335b7e8d4351e865f5bf61
GYassine2000 <garraouiyassine86@gmail.com>
2024-03-10 14:42:33 +0100

    ajouter le code de GPS

---
+++ guide-alzheimer/guide-alzheimer.ino
@@ -0,0 +1,74 @@
+#include <Adafruit_GPS.h>    //Install the adafruit GPS library
+#include <SoftwareSerial.h> //Load the Software Serial library
+
+SoftwareSerial mySerial(0,1); //Initialize the Software Serial port
+Adafruit_GPS GPS(&mySerial); //Create the GPS Object
+
+String NMEA1; //Variable for first NMEA sentence
+String NMEA2; //Variable for second NMEA sentence
+String latitude;
+char c; //to read characters coming from the GPS
+
+void setup() {
+  
+  Serial.begin(115200); //Turn on serial monitor
+  GPS.begin(9600); //Turn on GPS at 9600 baud
+  GPS.sendCommand("$PGCMD,33,0*6D");  //Turn off antenna update nuisance data
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA); //Request RMC and GGA Sentences only
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); //Set update rate to 1 hz
+  delay(1000); 
+
+}
+
+void loop() {
+  
+  readGPS();
+
+  if(GPS.fix==1) { 
+
+   latitude=GPS.latitude;
+    GPS.lat;
+    GPS.longitude;
+    GPS.lon;
+    GPS.altitude;
+
+  }
+  
+}
+
+void readGPS() {
+  
+  clearGPS();
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  NMEA1=GPS.lastNMEA();
+  
+   while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  NMEA2=GPS.lastNMEA();
+  
+  Serial.println(NMEA1);
+  Serial.println(NMEA2);
+  Serial.println("");
+  
+}
+
+void clearGPS() {  //Clear old and corrupt data from serial port 
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+  while(!GPS.newNMEAreceived()) { //Loop until you have a good NMEA sentence
+    c=GPS.read();
+  }
+  GPS.parse(GPS.lastNMEA()); //Parse that last good NMEA sentence
+}
